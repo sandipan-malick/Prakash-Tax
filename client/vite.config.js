527ee1ca05a690a0202ade0f916f3c59ea5fea87
Sandipan Malick <malicksandipan44@gmail.com>
2026-01-03 18:44:00 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss(),
   ],
-  server:{
-    proxy:{
-      "/api" :{
-        target: "http://localhost:5080",
-        changeOrigin : true,
-        secure: false,
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://prakash-tax.onrender.com',
+        changeOrigin: true,
+        secure: true,
       },
     },
   },
-});
+})
